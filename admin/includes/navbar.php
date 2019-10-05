c0cdefe14cfd75a4654e751f3702e23124333d51
jogindrakumar <joginder9868@outlook.com>
2019-10-05 11:35:13 +0530

    On branch master

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -13,22 +13,7 @@
       <li class="nav-item">
         <a class="nav-link text-white" href="../index.php">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link text-white" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-white" href="#">Pricing</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown link
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
+      
       
     </ul>
     
